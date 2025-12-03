9a2d9a805e0c249e30ca67feb1f73364d337e74a
maga-js <psonly214@gmail.com>
2025-12-03 17:15:15 +0400

    Create Mapdz.java

---
+++ Mapdz.java
@@ -0,0 +1,67 @@
+package map;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+public class Mapdz {
+    public static void main(String[] args) {
+        HashMap<String, Integer> jitelimagastana = new HashMap<>();
+        System.out.println("nomer 1 учитель извините но я не знаю ваш возраст поэтому то что в голову пришло написал");
+        System.out.println("=======================================");
+        jitelimagastana.put("maga", 13);
+        jitelimagastana.put("said", 13);
+        jitelimagastana.put("suel", 25);
+    jitelimagastana.put("casur", 15);
+    jitelimagastana.put("shamil", 16);
+jitelimagastana.put("maga2", 12);
+jitelimagastana.put("Zakir", 23);
+        System.out.println(jitelimagastana);
+        System.out.println("nomer 2");
+        System.out.println("=======================================");
+        LinkedHashMap<Integer, String> uroki = new LinkedHashMap<>();
+
+        uroki.put(3, "matematika");
+        uroki.put(5, "ruskiy");
+        uroki.put(1, "fizika");
+        uroki.put(4, "informatika");
+        uroki.put(2, "informatika");
+        System.out.println(uroki);
+        System.out.println("nomer 3");
+        System.out.println("=======================================");
+
+
+        TreeMap<Integer, String> igri = new TreeMap<>();
+        igri.put(1, "cs2, tolko cto igral");
+        igri.put(2,"rust");
+        igri.put(3, "detroit become a human, denek na nee netu poetomu 3");
+        igri.put(4, "Team fortres 2 ");
+        igri.put(5,"Bird game 3");
+        igri.put(6, "minecraft");
+        igri.put(7, "rdr 2");
+        igri.put(8, "gta 4");
+        System.out.println("nomer 4");
+        System.out.println("=======================================");
+        System.out.println("izvinite ne znayu kak sdelat");
+
+
+        System.out.println("nomer 5");
+        System.out.println("=======================================");
+
+        NavigableMap<Integer> numbers = new TreeMap();
+        System.out.println("погоди те ка, а как мы ему валью напишем");
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
